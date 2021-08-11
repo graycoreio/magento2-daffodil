07535f2a300ea0cdb339d510b627195573679eb3
Damien Retzinger <damienwebdev@gmail.com>
2021-08-11 11:21:16 -0600

    feat(sitemap): make sitemap use Daffodil url

---
+++ Sitemap/Sitemap.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * Copyright © Graycore, LLC. All rights reserved.
+ * See LICENSE.md for details.
+ */
+
+declare (strict_types = 1);
+
+namespace Graycore\Daffodil\Sitemap;
+
+use Magento\Framework\UrlInterface;
+use Magento\Sitemap\Model\Sitemap as MagentoSitemap;
+
+class Sitemap extends MagentoSitemap
+{
+
+    /**
+     * @var \Graycore\Daffodil\Configuration\Configuration
+     */
+    private $_configuration;
+
+    /**
+     * Initialize dependencies.
+     *
+     * @param \Magento\Framework\Model\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Framework\Escaper $escaper
+     * @param \Magento\Sitemap\Helper\Data $sitemapData
+     * @param \Magento\Framework\Filesystem $filesystem
+     * @param ResourceModel\Catalog\CategoryFactory $categoryFactory
+     * @param ResourceModel\Catalog\ProductFactory $productFactory
+     * @param ResourceModel\Cms\PageFactory $cmsFactory
+     * @param \Magento\Framework\Stdlib\DateTime\DateTime $modelDate
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Framework\App\RequestInterface $request
+     * @param \Magento\Framework\Stdlib\DateTime $dateTime
+     * @param \Magento\Framework\Model\ResourceModel\AbstractResource|null $resource
+     * @param \Magento\Framework\Data\Collection\AbstractDb|null $resourceCollection
+     * @param array $data
+     * @param DocumentRoot|null $documentRoot
+     * @param ItemProviderInterface|null $itemProvider
+     * @param SitemapConfigReaderInterface|null $configReader
+     * @param \Magento\Sitemap\Model\SitemapItemInterfaceFactory|null $sitemapItemFactory
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Escaper $escaper,
+        \Magento\Sitemap\Helper\Data $sitemapData,
+        \Magento\Framework\Filesystem $filesystem,
+        \Magento\Sitemap\Model\ResourceModel\Catalog\CategoryFactory $categoryFactory,
+        \Magento\Sitemap\Model\ResourceModel\Catalog\ProductFactory $productFactory,
+        \Magento\Sitemap\Model\ResourceModel\Cms\PageFactory $cmsFactory,
+        \Magento\Framework\Stdlib\DateTime\DateTime $modelDate,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\App\RequestInterface $request,
+        \Magento\Framework\Stdlib\DateTime $dateTime,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = [],
+        \Magento\Config\Model\Config\Reader\Source\Deployed\DocumentRoot $documentRoot = null,
+        ItemProviderInterface $itemProvider = null,
+        SitemapConfigReaderInterface $configReader = null,
+        \Magento\Sitemap\Model\SitemapItemInterfaceFactory $sitemapItemFactory = null,
+        \Graycore\Daffodil\Configuration\Configuration $configuration
+    ) {
+        parent::__construct(
+            $context,
+            $registry,
+            $escaper,
+            $sitemapData,
+            $filesystem,
+            $categoryFactory,
+            $productFactory,
+            $cmsFactory,
+            $modelDate,
+            $storeManager,
+            $request,
+            $dateTime,
+            $resource,
+            $resourceCollection,
+            $data,
+            $documentRoot,
+            $itemProvider,
+            $configReader,
+            $sitemapItemFactory,
+        );
+
+        $this->_configuration = $configuration;
+    }
+
+    /**
+     * Get the base url for the sitemap from Daffodil configuration as opposed
+     * to Magento store configuration.
+     */
+    protected function _getStoreBaseUrl($type = UrlInterface::URL_TYPE_LINK)
+    {
+        if ($this->_configuration->isActive()) {
+            return rtrim($this->_configuration->getDaffodilUrl(), '/') . '/';
+        }
+        return parent::_getStoreBaseUrl($type);
+    }
+}
